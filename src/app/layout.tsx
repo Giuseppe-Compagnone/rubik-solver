98683fa1a5c6a31039b1b0aaa155871d2bff0bf9
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2025-04-12 17:47:49 +0300

    Add title to the head element in RootLayout for improved SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Rubik Solver</title>
+      </head>
       <body>
         <ProviderWrapper>{children}</ProviderWrapper>
       </body>
